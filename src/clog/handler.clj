c2cffcd6bef01e015b3565b5684bd105184834e1
Danny Wilson <dannywilson32@gmail.com>
2019-11-06 21:58:00 +0000

    added build menu from dir

--- src/clog/handler.clj
+++ src/clog/handler.clj
@@ -1,16 +1,28 @@
 (ns clog.handler
   (:require
    [clojure.java.io :as io]
+   [clojure.string :as str]
    [compojure.core :refer :all]
    [compojure.route :as route]
    [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
    [hiccup.core :as markup]
    [markdown-to-hiccup.core :as m]))
 
+(defn get-page-list []
+  (map
+   (fn [path]
+     (str/replace path ".md" ""))
+   (.list (io/file "_pages"))))
+
+(defn build-menu [page-list]
+  (map (fn [page]
+         [:a {:href (str "/" page)} page ]) page-list))
+
 (defn page->path [page] (str "_pages/" page ".md"))
 
 (defn layout [page]
   (markup/html [:header "danwlsn clogs"]
+               (build-menu (get-page-list))
                (m/file->hiccup
                 (if (.exists (io/file page))
                   page (page->path "404")))
