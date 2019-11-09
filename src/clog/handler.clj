71dbd9735a0dddcc875c54d5cdcd129109dcae1a
Danny Wilson <dannywilson32@gmail.com>
2019-11-09 09:28:00 +0000

    lots of changes

--- src/clog/handler.clj
+++ src/clog/handler.clj
@@ -12,36 +12,49 @@
   (map
    (fn [path]
      (str/replace path ".md" ""))
-   (.list (io/file dir))))
+   (.list (io/file (str "_"  dir)))))
 
-(defn build-menu [page-list path]
-  (map (fn [page]
-         [:a {:href (str "/" path page)} page ]) page-list))
+(defn build-menu [page-list path formatter]
+  (->> (map
+        (fn [page]
+          [:li  [:a {:href (str "/" path page)} (formatter page) ]]
+          ) page-list) (into [:ul])))
 
 (defn page->path [page path] (str path page ".md"))
 
+(defn remove-date [title]
+  (str/replace (nth (str/split title #"_") 1) "-" " "))
+
 (defn posts []
-  (build-menu (get-page-list "_posts") "posts/"))
+  (build-menu (get-page-list "posts") "posts/" remove-date))
 
 (defn layout [content]
   (markup/html [:header "danwlsn clogs"]
-               (build-menu (get-page-list "_pages") "/")
-               (content)
+               (build-menu
+                (concat (get-page-list "pages") '("posts")) "" str/capitalize)
+               content
                [:footer "follow me on twitter"]))
 
+(defn load-markdown [page dir]
+  (->> (if (.exists (io/file (page->path page dir)))
+         (page->path page dir) (page->path "404" "_errors/"))
+       (m/file->hiccup)
+       (m/component)))
+
 (defn static [page]
-  (m/file->hiccup
-   (if (.exists (io/file (page->path page "_pages/")))
-     (page->path page "_pages/") (page->path "404" "_pages/"))))
+  (load-markdown page "_pages/"))
+
+(defn blog [page]
+  (load-markdown page "_posts/"))
 
 (defn render-page
   ([render] (layout render)))
 
 (defroutes app-routes
-  (GET "/" [] (render-page (static "index")))
-  (GET "/posts" [] (render-page posts))
-  (GET "/posts/:post" [post] (render-page (static "index")))
-  (GET "/:page" [page] (render-page (static "index"))))
+  (GET "/" [] (render-page (static "home")))
+  (GET "/posts" [] (render-page (posts)))
+  (GET "/posts/:post" [post] (render-page (blog post)))
+  (GET "/:page" [page] (render-page (static page))))
 
 (def app
   (wrap-defaults app-routes site-defaults))
