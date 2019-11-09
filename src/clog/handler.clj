60f175e7072a8c5843f04348115499701b5e53f6
Danny Wilson <dannywilson32@gmail.com>
2019-11-09 18:31:53 +0000

    basic styling

--- src/clog/handler.clj
+++ src/clog/handler.clj
@@ -19,9 +19,16 @@
   (concat (get-page-list "pages") extras))
 
 (defn build-menu [page-list path formatter]
-  (into [:ul]
+  (into [:div {:class "db dtc-l v-mid w-100 w-75-l tc tr-l"}]
         (map (fn [page]
-               [:li  [:a {:href (str "/" path page)} (formatter page) ]]
+               [:a {:href (str "/" path page) :class "link dim dark-gray f6 f5-l dib mr3 mr4-l"} (formatter page) ]
+               ) page-list)))
+
+(defn build-link-list [page-list path formatter]
+  (into [:ul {:class "list pl0"}]
+        (map (fn [page]
+               [:li {:class "lh-copy pv1"}
+                [:a {:href (str "/" path page) :class "link"} (formatter page) ]]
                ) page-list)))
 
 (defn page->path [page path] (str path page ".md"))
@@ -30,20 +37,33 @@
   (str/replace (nth (str/split title #"_") 1) "-" " "))
 
 (defn posts []
-  (vec (list [:h1 {} "posts"]
-             (build-menu (get-page-list "posts") "posts/" remove-date))))
+  (vec [:div {:class "w100"} (build-link-list (get-page-list "posts") "posts/" remove-date)]))
 
 (defn layout [content]
   (markup/html
    (page/html5
     [:head
-     [:title "Dan Wilson CLOGS"]
-     (page/include-css "https://unpkg.com/tachyons@4.10.0/css/tachyons.min.css")]
-    [:header "danwlsn clogs"]
-    (build-menu
-     (nav '("posts")) "" str/capitalize)
-    content
-    [:footer "follow me on twitter"])))
+     [:title "Dan Wilson CLOGS"]]
+    (page/include-css "https://unpkg.com/tachyons@4.10.0/css/tachyons.min.css")
+    [:body {:class "sans-serif"}
+     [:div {:class "mw9 center ph3-ns"}
+      [:div {:class "cf ph2-ns"}
+       [:div {:class "fl w-100 pa2"}
+        [:nav {:class "db dt-l w-100 border-box pa3 ph5-l"}
+         [:a {:class "db dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2 mb0-l"
+              :href "/"}
+          [:h1 "danwlsn's clog"]
+          (build-menu
+           (nav '("posts")) "" str/capitalize)]]
+        content
+        [:footer {:class "ph3 ph4-ns pv4 bt b--black-10 black-70"}
+         [:div
+          [:a
+           {:class "f6 dib pr2 mid-gray dim"
+            :href "https://github.com/danwlsn"} "github"]
+          [:a
+           {:class "f6 dib pr2 mid-gray dim"
+            :href "https://twitter.com/danwlsn"} "twitter"]]]]]]])))
 
 (defn load-markdown [page dir]
 (->> (if (.exists (io/file (page->path page dir)))
@@ -52,7 +72,7 @@
      (m/component)))
 
 (defn static [page]
-(load-markdown page "_pages/"))
+  (load-markdown page "_pages/"))
 
 (defn blog [page]
 (load-markdown page "_posts/"))
